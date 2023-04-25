1c21e6b6550af6580924443ae85e6b45cb66ced6
Thomas997 <124135942+Thomas997@users.noreply.github.com>
2023-04-25 12:12:15 +0200

    Basic fraim

--- Donkey Kong/Form1.Designer.cs
+++ Donkey Kong/Form1.Designer.cs
@@ -29,12 +29,179 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.Enemy = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtScore
+            // 
+            this.txtScore.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(380, 9);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(274, 84);
+            this.txtScore.TabIndex = 0;
+            this.txtScore.Text = "Score: 0";
+            this.txtScore.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Pink;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 734);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(656, 30);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "platform";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Pink;
+            this.pictureBox2.Location = new System.Drawing.Point(-2, 616);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(527, 30);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "platform";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Pink;
+            this.pictureBox3.Location = new System.Drawing.Point(127, 507);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(527, 30);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "platform";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Pink;
+            this.pictureBox4.Location = new System.Drawing.Point(-2, 383);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(527, 30);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "platform";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Pink;
+            this.pictureBox5.Location = new System.Drawing.Point(127, 257);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(527, 30);
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Tag = "platform";
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Pink;
+            this.pictureBox6.Location = new System.Drawing.Point(-2, 143);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(527, 30);
+            this.pictureBox6.TabIndex = 6;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Tag = "platform";
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.DarkBlue;
+            this.Player.Location = new System.Drawing.Point(99, 688);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(30, 40);
+            this.Player.TabIndex = 7;
+            this.Player.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.DarkGreen;
+            this.pictureBox7.Location = new System.Drawing.Point(-2, 73);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(59, 73);
+            this.pictureBox7.TabIndex = 8;
+            this.pictureBox7.TabStop = false;
+            // 
+            // Enemy
+            // 
+            this.Enemy.BackColor = System.Drawing.Color.Brown;
+            this.Enemy.Location = new System.Drawing.Point(160, 106);
+            this.Enemy.Name = "Enemy";
+            this.Enemy.Size = new System.Drawing.Size(30, 40);
+            this.Enemy.TabIndex = 9;
+            this.Enemy.TabStop = false;
+            this.Enemy.Tag = "Enemy";
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.MainGameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(654, 761);
+            this.Controls.Add(this.Enemy);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.Player);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtScore);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Name = "Form1";
+            this.Text = "Donkey Kong";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox Enemy;
+        private System.Windows.Forms.Timer GameTimer;
     }
 }
 
--- Donkey Kong/Form1.cs
+++ Donkey Kong/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,163 @@ namespace Donkey_Kong
 {
     public partial class Form1 : Form
     {
+        //Variabelen
+        bool goLeft, goRight, jumping, isGameOver;
+
+        int jumpSpeed;
+        int force;
+        int score = 0;
+        int playerSpeed = 7;
+
+        int horizontalSpeed = 5;
+        int verticalSpeed = 3;
+
+        int barrel = 5;
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //Dit is de timer
+        private void MainGameTimerEvent(object sender, EventArgs e)
+        {
+            txtScore.Text = "Score: " + score;
+
+            //zwaartekracht
+            Player.Top += jumpSpeed;
+
+            if(goLeft == true)
+            {
+                Player.Left -= playerSpeed;
+            }
+            
+            if(goRight == true)
+            {
+                Player.Left += playerSpeed;
+            }
+
+            //Dit zorgt er voor dat de game weet hoe hoog je springt en het checked of de force 0 is dus dat je mag springen
+            if(jumping == true && force < 0)
+            {
+                jumping = false;
+            }
+
+            //Als het springen begonnen is dan moeten de variabelen naar benden zodat het ook stopt anders mag het gewoon standaard zodat je kunt springen
+            if(jumping == true)
+            {
+                jumpSpeed = -8;
+                force -= 1;
+            }
+            else
+            {
+                jumpSpeed = 10;
+            }
+
+            //Hier gebruiken we de tag van de platformen zodat de game dit als een vast object ziet waar je kunt op staan
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox)
+                {
+
+                    if((string)x.Tag == "platform")
+                    {
+
+                        if(Player.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            force = 8;
+                            Player.Top = x.Top - Player.Height; 
+                        }
+
+                        x.BringToFront();
+                    }
+
+
+
+                }
+            }
+
+
+        }
+
+        //Dit is als er een toets is in gedrukt
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            //Dit is de code voor de linker toets ingedrukt
+            if(e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            
+            //Dit is de code voor de rechter toets ingedrukt
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+
+            //Dit is de code voor de spaciebalk en als je springt
+            if(e.KeyCode == Keys.Space && jumping == false)
+            {
+                jumping = true;
+            }
+        }
+
+        //Dit is als er een toets is los gelaten
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            //Dit is de code voor de linker toets als losgelaten
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+
+            //Dit is de code voor de rechter toets als losgelaten
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+
+            //Dit is de code voor eindigen van het springen als dat al gebeurt is 
+            if(jumping == true)
+            {
+                jumping = false;
+            }
+
+            //Als er op enter wordt ge drukt herstart de game
+            if(e.KeyCode == Keys.Enter && isGameOver == true)
+            {
+                RestartGame();
+            }
+        }
+
+        //Deze functie herstart het spel en reset de variabelen
+        private void RestartGame()
+        {
+            jumping = false;
+            goLeft = false;
+            goRight = false;
+            isGameOver = false;
+            score = 0;
+
+            txtScore.Text = "Score: " + score;
+
+            foreach(Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Visible == false)
+                {
+                    x.Visible = true;
+                }
+            }
+
+            //Reset the position of player, platform and enemies
+
+            Player.Left = 99;
+            Player.Top = 688;
+
+            Enemy.Left = 160;
+
+            GameTimer.Start();
         }
     }
 }
