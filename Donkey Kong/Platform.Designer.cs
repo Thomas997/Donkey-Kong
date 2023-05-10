7835e36a53f2e9e084234e8fbd4f4dde87aeb162
ConorV12 <conor.verbeek.nautica@gmail.com>
2023-05-10 11:35:42 +0200

    Pull fix

---
+++ Donkey Kong/Platform.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace Donkey_Kong
+{
+    partial class Platform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.platform1 = new System.Windows.Forms.PictureBox();
+            this.platform2 = new System.Windows.Forms.PictureBox();
+            this.platform3 = new System.Windows.Forms.PictureBox();
+            this.platform5 = new System.Windows.Forms.PictureBox();
+            this.platform4 = new System.Windows.Forms.PictureBox();
+            this.platform6 = new System.Windows.Forms.PictureBox();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.rightSidewall = new System.Windows.Forms.PictureBox();
+            this.leftSidewall = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.pictureBox13 = new System.Windows.Forms.PictureBox();
+            this.pictureBox14 = new System.Windows.Forms.PictureBox();
+            this.txtHighscore = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblHighscore = new System.Windows.Forms.Label();
+            this.BarrelTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.platform1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightSidewall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftSidewall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtScore
+            // 
+            this.txtScore.Font = new System.Drawing.Font("Kongtext", 24F);
+            this.txtScore.Location = new System.Drawing.Point(-14, 52);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(274, 43);
+            this.txtScore.TabIndex = 0;
+            this.txtScore.Text = "0";
+            this.txtScore.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // platform1
+            // 
+            this.platform1.BackColor = System.Drawing.Color.Pink;
+            this.platform1.Location = new System.Drawing.Point(201, 911);
+            this.platform1.Name = "platform1";
+            this.platform1.Size = new System.Drawing.Size(656, 29);
+            this.platform1.TabIndex = 1;
+            this.platform1.TabStop = false;
+            this.platform1.Tag = "platform";
+            // 
+            // platform2
+            // 
+            this.platform2.BackColor = System.Drawing.Color.Pink;
+            this.platform2.Location = new System.Drawing.Point(201, 776);
+            this.platform2.Name = "platform2";
+            this.platform2.Size = new System.Drawing.Size(527, 29);
+            this.platform2.TabIndex = 2;
+            this.platform2.TabStop = false;
+            this.platform2.Tag = "platform";
+            // 
+            // platform3
+            // 
+            this.platform3.BackColor = System.Drawing.Color.Pink;
+            this.platform3.Location = new System.Drawing.Point(330, 640);
+            this.platform3.Name = "platform3";
+            this.platform3.Size = new System.Drawing.Size(527, 30);
+            this.platform3.TabIndex = 3;
+            this.platform3.TabStop = false;
+            this.platform3.Tag = "platform";
+            // 
+            // platform5
+            // 
+            this.platform5.BackColor = System.Drawing.Color.Pink;
+            this.platform5.Location = new System.Drawing.Point(330, 370);
+            this.platform5.Name = "platform5";
+            this.platform5.Size = new System.Drawing.Size(527, 30);
+            this.platform5.TabIndex = 4;
+            this.platform5.TabStop = false;
+            this.platform5.Tag = "platform";
+            // 
+            // platform4
+            // 
+            this.platform4.BackColor = System.Drawing.Color.Pink;
+            this.platform4.Location = new System.Drawing.Point(201, 505);
+            this.platform4.Name = "platform4";
+            this.platform4.Size = new System.Drawing.Size(527, 30);
+            this.platform4.TabIndex = 5;
+            this.platform4.TabStop = false;
+            this.platform4.Tag = "platform";
+            // 
+            // platform6
+            // 
+            this.platform6.BackColor = System.Drawing.Color.Pink;
+            this.platform6.Location = new System.Drawing.Point(201, 237);
+            this.platform6.Name = "platform6";
+            this.platform6.Size = new System.Drawing.Size(527, 30);
+            this.platform6.TabIndex = 6;
+            this.platform6.TabStop = false;
+            this.platform6.Tag = "platform";
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.DarkBlue;
+            this.Player.Location = new System.Drawing.Point(256, 875);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(30, 40);
+            this.Player.TabIndex = 7;
+            this.Player.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.DarkGreen;
+            this.pictureBox7.Location = new System.Drawing.Point(201, 164);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(59, 73);
+            this.pictureBox7.TabIndex = 8;
+            this.pictureBox7.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.MainGameTimerEvent);
+            // 
+            // rightSidewall
+            // 
+            this.rightSidewall.Location = new System.Drawing.Point(855, 156);
+            this.rightSidewall.Name = "rightSidewall";
+            this.rightSidewall.Size = new System.Drawing.Size(217, 784);
+            this.rightSidewall.TabIndex = 10;
+            this.rightSidewall.TabStop = false;
+            this.rightSidewall.Tag = "sidewall";
+            // 
+            // leftSidewall
+            // 
+            this.leftSidewall.Location = new System.Drawing.Point(-5, 156);
+            this.leftSidewall.Name = "leftSidewall";
+            this.leftSidewall.Size = new System.Drawing.Size(207, 784);
+            this.leftSidewall.TabIndex = 11;
+            this.leftSidewall.TabStop = false;
+            this.leftSidewall.Tag = "sidewall";
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox10.Location = new System.Drawing.Point(688, 776);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(40, 139);
+            this.pictureBox10.TabIndex = 12;
+            this.pictureBox10.TabStop = false;
+            this.pictureBox10.Tag = "ladder";
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox11.Location = new System.Drawing.Point(579, 640);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(40, 139);
+            this.pictureBox11.TabIndex = 13;
+            this.pictureBox11.TabStop = false;
+            this.pictureBox11.Tag = "ladder";
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox12.Location = new System.Drawing.Point(435, 505);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(40, 139);
+            this.pictureBox12.TabIndex = 14;
+            this.pictureBox12.TabStop = false;
+            this.pictureBox12.Tag = "ladder";
+            // 
+            // pictureBox13
+            // 
+            this.pictureBox13.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox13.Location = new System.Drawing.Point(643, 370);
+            this.pictureBox13.Name = "pictureBox13";
+            this.pictureBox13.Size = new System.Drawing.Size(40, 139);
+            this.pictureBox13.TabIndex = 15;
+            this.pictureBox13.TabStop = false;
+            this.pictureBox13.Tag = "ladder";
+            // 
+            // pictureBox14
+            // 
+            this.pictureBox14.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox14.Location = new System.Drawing.Point(407, 237);
+            this.pictureBox14.Name = "pictureBox14";
+            this.pictureBox14.Size = new System.Drawing.Size(40, 139);
+            this.pictureBox14.TabIndex = 16;
+            this.pictureBox14.TabStop = false;
+            this.pictureBox14.Tag = "ladder";
+            // 
+            // txtHighscore
+            // 
+            this.txtHighscore.Font = new System.Drawing.Font("Kongtext", 24F);
+            this.txtHighscore.Location = new System.Drawing.Point(364, 52);
+            this.txtHighscore.Name = "txtHighscore";
+            this.txtHighscore.Size = new System.Drawing.Size(404, 39);
+            this.txtHighscore.TabIndex = 17;
+            this.txtHighscore.Text = "0";
+            this.txtHighscore.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblScore
+            // 
+            this.lblScore.Font = new System.Drawing.Font("Kongtext", 24F);
+            this.lblScore.ForeColor = System.Drawing.Color.Red;
+            this.lblScore.Location = new System.Drawing.Point(12, 9);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(224, 43);
+            this.lblScore.TabIndex = 18;
+            this.lblScore.Text = "SCORE";
+            this.lblScore.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblHighscore
+            // 
+            this.lblHighscore.Font = new System.Drawing.Font("Kongtext", 24F);
+            this.lblHighscore.ForeColor = System.Drawing.Color.Red;
+            this.lblHighscore.Location = new System.Drawing.Point(401, 9);
+            this.lblHighscore.Name = "lblHighscore";
+            this.lblHighscore.Size = new System.Drawing.Size(345, 43);
+            this.lblHighscore.TabIndex = 19;
+            this.lblHighscore.Text = "HIGH SCORE";
+            this.lblHighscore.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // BarrelTimer
+            // 
+            this.BarrelTimer.Enabled = true;
+            this.BarrelTimer.Interval = 3000;
+            this.BarrelTimer.Tick += new System.EventHandler(this.BarrelTimer_Tick);
+            // 
+            // Platform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(1064, 941);
+            this.Controls.Add(this.lblHighscore);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.txtHighscore);
+            this.Controls.Add(this.pictureBox14);
+            this.Controls.Add(this.pictureBox13);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.leftSidewall);
+            this.Controls.Add(this.rightSidewall);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.Player);
+            this.Controls.Add(this.platform6);
+            this.Controls.Add(this.platform4);
+            this.Controls.Add(this.platform5);
+            this.Controls.Add(this.platform3);
+            this.Controls.Add(this.platform2);
+            this.Controls.Add(this.platform1);
+            this.Controls.Add(this.txtScore);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Platform";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Donkey Kong";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.platform1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.platform6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightSidewall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftSidewall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.PictureBox platform1;
+        private System.Windows.Forms.PictureBox platform2;
+        private System.Windows.Forms.PictureBox platform3;
+        private System.Windows.Forms.PictureBox platform5;
+        private System.Windows.Forms.PictureBox platform4;
+        private System.Windows.Forms.PictureBox platform6;
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.PictureBox rightSidewall;
+        private System.Windows.Forms.PictureBox leftSidewall;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.PictureBox pictureBox13;
+        private System.Windows.Forms.PictureBox pictureBox14;
+        private System.Windows.Forms.Label txtHighscore;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblHighscore;
+        private System.Windows.Forms.Timer BarrelTimer;
+    }
+}
+
